0b03be0fbb584592236fe0a0cc0b2c9e2b776991
azizovic-endava <aleksandar.zizovic@endava.com>
2019-05-22 03:39:17 +0200

    Prva nedovrsena verzija

---
+++ DomaciZadatak1/src/klasniTipovi/Prijava.java
@@ -0,0 +1,51 @@
+package klasniTipovi;
+
+import java.util.Date;
+
+public class Prijava {
+
+	private int sifraPredmeta;
+	private String brojIndeksa;
+	private Date datumPolaganja;
+	private int ocena;
+	
+	public Prijava(int sifraPredmeta, String brojIndeksa, Date datumPolaganja, int ocena) {
+		super();
+		this.sifraPredmeta = sifraPredmeta;
+		this.brojIndeksa = brojIndeksa;
+		this.datumPolaganja = datumPolaganja;
+		this.ocena = ocena;
+	}
+
+	public int getSifraPredmeta() {
+		return sifraPredmeta;
+	}
+
+	public void setSifraPredmeta(int sifraPredmeta) {
+		this.sifraPredmeta = sifraPredmeta;
+	}
+
+	public String getBrojIndeksa() {
+		return brojIndeksa;
+	}
+
+	public void setBrojIndeksa(String brojIndeksa) {
+		this.brojIndeksa = brojIndeksa;
+	}
+
+	public Date getDatumPolaganja() {
+		return datumPolaganja;
+	}
+
+	public void setDatumPolaganja(Date datumPolaganja) {
+		this.datumPolaganja = datumPolaganja;
+	}
+
+	public int getOcena() {
+		return ocena;
+	}
+
+	public void setOcena(int ocena) {
+		this.ocena = ocena;
+	}
+}
